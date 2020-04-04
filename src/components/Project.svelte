7aa7a509bd20d927397ee80bc610ebba3d0c478f
TomFrink <tomfrink11@hotmail.com>
2020-04-04 11:53:39 -0400

    Added Project Component to Projects Page

---
+++ src/components/Project.svelte
@@ -0,0 +1,25 @@
+<script>
+	export let name;
+	export let tech;
+	export let type;
+	export let website;
+	export let github;
+</script>
+
+	<h2>
+		<code>{name}</code> is a {type} project
+	</h2>
+	Tech used:
+	<ul>
+		{#each tech as {packageName, id}, i}
+		<li target="_blank">
+			<a href="https://www.npmjs.com/package/{id}">
+				{packageName}
+			</a>
+		</li>
+		{/each}
+	</ul>
+
+	<a href={website}>Click Here</a> to see {name}
+	<br />
+	<a href= 'https://github.com/TomFrink/{github}'>Click Here</a> to see {name}'s code on GitHub
--- src/routes/projects.svelte
+++ src/routes/projects.svelte
@@ -1,5 +1,23 @@
+<script>
+	import Project from '../components/Project.svelte';
+
+	const projectdetails = {
+		name: 'Stoic Page',
+		type: 'Vue',
+		tech: [
+			{packageName: 'Vue.js',id:'vue'},
+			{packageName: 'Google Firebase', id:'firebase'},
+			{packageName: 'Vuefire', id:'vuefire'}
+		],
+		website: 'https://stoic.page',
+		github: 'StoicPage'
+  };
+</script>
+
 <svelte:head>
   <title>Projects</title>
 </svelte:head>
 
 <h1>Projects</h1>
+
+<Project {...projectdetails}/>
